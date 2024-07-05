eb422751517ea16e05386365789fe78e40ec71a1
Adrian Heffley <aheffley@student.neumont.edu>
2024-07-05 14:27:24 -0600

    fireworks

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,6 +1,9 @@
 #include "Renderer.h"
 #include "Vector2.h"
 #include "Input.h"
+#include "Particle.h"
+#include "Random.h"
+#include "ETime.h"
 
 #include <SDL.h>
 #include <iostream>
@@ -17,6 +20,8 @@ int main(int argc, char* argv[])
 	Input input;
 	input.Initialize();
 
+	Time time;
+
 	Vector2 v1{ 400, 300 };
 	Vector2 v2{ 500, 400 };
 
@@ -26,6 +31,12 @@ int main(int argc, char* argv[])
 		points.push_back(Vector2{ rand() % 800, rand() % 600});
 	}*/
 
+	std::vector<Particle> particles;
+	for (int i = 0; i < 0; i++)
+	{
+		particles.push_back(Particle{ {rand() % 800, rand() % 800}, { randomf(100, 300), 0.0f}});
+	}
+
 	// main loop
 	bool quit = false;
 	while (!quit)
@@ -33,6 +44,9 @@ int main(int argc, char* argv[])
 		// input
 		// update
 		// draw
+
+		time.Tick();
+		std::cout << time.GetTime() << std::endl;
 
 		// INPUT
 		input.Update();
@@ -44,9 +58,23 @@ int main(int argc, char* argv[])
 
 		// UPDATE
 		Vector2 mousePosition = input.GetMousePosition();
+
+		if (input.GetMouseButtonDown(0))
+		{
+			particles.push_back(Particle{ {mousePosition}, {randomf(-100, 100), randomf(-100, 100)}, {randomf(1, 5)} });
+		}
 		//std::cout << mousePosition.x << " " << mousePosition.y << std::endl;
 
-		if (input.GetMouseButtonDown(0) && !input.GetPreviousMouseButtonDown(0))
+		for (Particle& particle : particles)
+		{
+			particle.Update(time.GetDeltaTime());
+
+			if (particle.position.x > 800) particle.position.x = 0;
+			if (particle.position.x < 0) particle.position.x = 800;
+			if (particle.lifespan != 0) particle.lifespan -= time.GetDeltaTime();
+		}
+
+		/*if (input.GetMouseButtonDown(0) && !input.GetPreviousMouseButtonDown(0))
 		{
 			std::cout << "mouse pressed\n";
 			points.push_back(mousePosition);
@@ -58,7 +86,7 @@ int main(int argc, char* argv[])
 			if (distance > 5) points.push_back(mousePosition);
 
 			points.push_back(mousePosition);
-		}
+		}*/
 
 		// [p, p, p, p]
 		/*Vector2 speed{ 0.1f, -0.1f };
@@ -73,7 +101,15 @@ int main(int argc, char* argv[])
 		renderer.BeginFrame();
 
 		////draw line
-		//renderer.SetColor(255, 255, 255, 0);
+		renderer.SetColor(255, 255, 255, 0);
+		for (Particle particle : particles)
+		{
+			if (particle.lifespan >= 0)
+			{
+				particle.Draw(renderer);
+			}
+		}
+
 		/*renderer.DrawLine(300, 250, 500, 250);
 
 		renderer.SetColor(255, 255, 255, 0);
@@ -84,11 +120,11 @@ int main(int argc, char* argv[])
 
 		//renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
 
-		for (int i = 0; points.size() > 1 && i < points.size() - 1; i++)
+		/*for (int i = 0; points.size() > 1 && i < points.size() - 1; i++)
 		{
 			renderer.SetColor(255, 255, 255, 0);
 			renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-		}
+		}*/
 
 		/*for (int i = 0; i < 25; i++)
 		{
